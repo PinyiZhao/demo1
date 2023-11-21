838a68ddf029682759c83595bd4b73103ad6e89b
Shihao-Yu <shihaoy@usc.edu>
2023-11-21 00:19:14 -0800

    Good Luck

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def upd_command():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=8080, debug=True)
 
 # @app.route('/eventSearch')
 # def eventSearch():
