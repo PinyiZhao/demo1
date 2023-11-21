f48656d892e6990734e81de83e04c49e26e4d25f
Shihao-Yu <shihaoy@usc.edu>
2023-11-21 14:02:05 -0800

    Pasrse-Multiple-Add-Command,Return-Msg-For-CURD-Ops

--- static/script_ysh.js
+++ static/script_ysh.js
@@ -20,6 +20,7 @@ document.getElementById('run-btn').addEventListener('click', async function() {
 document.getElementById('select-btn').addEventListener('click', async function() {
   const code = document.getElementById('code-input').value;
   const response = await axios.get(runCommand, { params: {command: code}});
+  console.log(response);
   const res = response["data"];
   const tableHead = document.getElementById('table-header');
   const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
@@ -53,7 +54,7 @@ async function addBottonHandle() {
   console.log(addCommand)
   const response = await  axios.get(runAdd, {params: {command: addCommand}})
   console.log(response)
-  document.getElementById('console-output2').textContent = response["data"].length;
+  document.getElementById('console-output2').textContent = response["data"];
 }
 async function deleteBottonHandle() {
   const delString = document.getElementById('code-input2').value;
@@ -61,7 +62,7 @@ async function deleteBottonHandle() {
   console.log(delCommand)
   const response = await  axios.get(runDel, {params: {command: delCommand}})
   console.log(response)
-  document.getElementById('console-output2').textContent = response["data"].length;
+  document.getElementById('console-output2').textContent = response["data"];
 }
 async function updateBottonHandle() {
   const updString = document.getElementById('code-input2').value;
@@ -69,7 +70,7 @@ async function updateBottonHandle() {
   console.log(updCommand)
   const response = await  axios.get(runUpd, {params: {command: updCommand}})
   console.log(response)
-  document.getElementById('console-output2').textContent = response["data"].length;
+  document.getElementById('console-output2').textContent = response["data"];
 }
 
 addBotton.addEventListener("click", addBottonHandle);
